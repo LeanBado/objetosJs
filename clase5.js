e667431dd3a64dcbb1887a263eeffa2522173b33
LeanBado <leandro.bado.42@gmail.com>
2022-06-12 16:23:26 -0300

    clases 5 y 6

---
+++ clase5.js
@@ -0,0 +1,63 @@
+function isObject(subject) {
+    return typeof subject == "object"
+}
+function esUnArray(subject) {
+    return Array.isArray(subject);
+}
+
+function deepCopy(subject) {
+    let copySubject;
+    const subjectIsArray = esUnArray(subject);
+    const subjectIsObject = isObject(subject);
+
+  if(subjectIsArray) {
+        copySubject = [];
+    } else if(subjectIsObject) {
+        copySubject = {};
+    } else {
+        return subject;
+    }
+
+    
+    for(key in subject) {
+            const keyIsObject = isObject(subject[key]);
+            if(keyIsObject) {
+                copySubject[key] = deepCopy(subject[key]);
+    } else {
+        if(subjectIsArray) {
+            copySubject.push(subject[key]);
+        } else {
+            copySubject[key] = subject[key]
+            }
+        }
+    }
+    return copySubject;
+} 
+
+//abstracción con objetos sin prototipos
+
+const studentBase = {
+    name: undefined,
+    email: undefined,
+    age: undefined,
+    approvedCourses: undefined,
+    learningPaths: undefined,
+    socialMedia: {
+      facebook: undefined,
+      twitter: undefined,
+      instagram: undefined,
+    }
+  };
+  
+  const lea = deepCopy(studentBase);
+  Object.defineProperty(lea, "name", {
+      value: "Jorge",
+      writable: false, // en falso NO permite que se modifique el valor (por defecto viene en true)
+      configurable: false, //en false NO permite que se borre el elemento (por defecto viene en true)
+  })
+  //Object.seal(lea); //* Ninguna propiedad del objeto lea se podrá eliminar
+  console.log(Object.isSealed(lea)); //* Verifica si el objeto tiene todas sus propiedades protegidas, como por ejemplo con .seal
+  console.log(Object.isFrozen(lea)); //* Verifica si el objeto tiene todas sus propiedades protegidas y además no se pueden agregar otras
+lea.name = "patricia"
+delete lea.name
+  console.log(lea)
---
+++ clase6.js
@@ -0,0 +1,43 @@
+//factory pattern - RORO (Receive an Object, Return and Object) - función que sirva para crear objetos
+
+function create({
+    name = advertencia("name"),
+    twitter = "default",
+    edad,
+    facebook = "default",
+    email = advertencia("email"),
+    instagram,
+    cursosAprobados = [],
+} = {}){
+    return {
+        name,
+        edad,           //se puede evitar name: name, edad: edad, porque JS intuye que va a ser el mismo que el parametro pasado
+        email,          //si se quiere cambiar el nombre ahí si hay que ponerle un valor.
+        redes: {
+            twitter,
+            instagram,
+            facebook,
+        },
+        cursosAprobados
+    };
+}
+
+function advertencia (parametro){
+    throw new console.error("el " + parametro + " es obligatorio completarlo");
+}
+//al momento de pasar los parametros en una funcion normal, debería coincidir el orden de lo que se pone cuando se invoca la funcion
+// con el orden de cuando se creó la función, para no tener este problema y poder poner los datos mezclados, se usa el concepto de RORO
+//entonces al momento de crear la función se pasa un sólo argumento en forma de {} con los datos que tenga que completar adentro
+const lea = create({
+    edad: 29,
+    name: "Leandro",
+    instagram: "leainsta",
+    email: "leaEmail",
+
+})
+const lauti = create({
+    edad: 29,
+    name: "Lautaro",
+})
+console.log(lea);
+console.log(lauti)
